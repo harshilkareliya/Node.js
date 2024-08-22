358de349d67b65cbea2682be74ee2f9347288325
harshilkareliya <kareliyaharshil2111@gmail.com>
2024-08-22 16:42:11 +0530

    delete suceessfully

--- Day 1/config/db.js
+++ Day 1/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/F_TODO', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/F_TODO');
 
 const db = mongoose.connection
 
--- Day 1/index.js
+++ Day 1/index.js
@@ -6,13 +6,19 @@ const crudSchema = require('./module/crudSchema');
 app.use(express.urlencoded())
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
-    // res.end("Hello World!");
+app.get('/', async (req, res) => {
+    let data = await crudSchema.find({})
+    res.render('index', {data})
 })
 
-app.post('/insertData', (req, res) => {
-    console.log(req.body);
+app.post('/insertData', async (req, res) => {
+    const data = await crudSchema.create(req.body)
+    data ? res.redirect("back") : console.log('Data not Inserted')
+})
+
+app.get('/deleteData', async (req, res) => { 
+    const data = await crudSchema.findByIdAndDelete(req.query.id)
+    data? res.redirect("back") : console.log('Data not Deleted')  
 })
 
 app.listen(port, (err)=> {
